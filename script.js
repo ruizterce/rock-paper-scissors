9f6b178f3b525fd042f34bf4592294d220d668a0
ruiz.terce <ruiz.terce@gmail.com>
2023-03-28 13:51:18 +0200

    Add Pseudocode to script.js
    Add Pseudocode of the main algorithms to be used in script.js.

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("yo")
+//Initialize a game of 5 rounds. Play 5 rounds and determine the winner of the game.
+    //Declare a counter variable.
+    //Call a function to play a round and add 1 to counter.
+    //When counter reaches the desired number of rounds, call a function to show the winner.
+
+//Generate and register computer's choice (rock, paper OR scissors)
+    //Generate a random number between 0 and 1.
+        //If number < 0.333 register Rock.
+        //If number >= 0.333 and < 0.666 register Paper.
+        //If number >= 0.666 Register Scissors.
+
+//Prompt user for input and register user's choice (rock, paper OR scissors).
+
+//Play a round - Call computer's choice and user's choice functions, process and register the winner of the round.
+        //If computer's choice is Rock.
+            //If player's choice is Rock, result is tie round.
+            //If player's choice is Paper, result is player wins round.
+            //If player's choice is Scissors, resilt is player loses round.
+            //If player's choice is none of the above, prompt user for a new choice.
+        //If computer's coice is Scissors.
+            //If player's choice is Rock, result is player wins round.
+            //If player's choice is Paper, result is player loses round.
+            //If player's choice is Scissors, resilt is tie round.
+            //If player's choice is none of the above, prompt user for a new choice.
+        //If computer's coice is Paper.
+            //If player's choice is Rock, result is player loses round.
+            //If player's choice is Paper, result is tie round.
+            //If player's choice is Scissors, result is player wins round.
+            //If player's choice is none of the above, prompt user for a new choice.
+        
