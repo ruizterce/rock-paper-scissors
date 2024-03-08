a219c861af1f3df596ecc411aa4eda4a86046041
ruiz.terce <ruiz.terce@gmail.com>
2024-03-08 18:10:23 +0100

    Add basic html/js UI to the game
    The game was played through browser prompts and
    console messages, without possibility to replay.
    This commit adds basic html/js UI with buttons,
    text elements, game reset functionality and adapts
    game logic to click event listeners.

--- script.js
+++ script.js
@@ -1,36 +1,128 @@
-//Run main function
+
+let getResult;
+let playerScore = 0;
+let computerScore = 0;
+let resultText = document.querySelector('#roundResult');
+let playerScoreDisplay = document.querySelector('#playerScore');
+let computerScoreDisplay = document.querySelector('#computerScore');
+let gameResultDisplay = document.querySelector('#gameResult');
+let playerSelectionDisplay = document.querySelector('#playerSelectionDisplay');
+let computerSelectionDisplay = document.querySelector('#computerSelectionDisplay');
+
+//Run main function to play a game of Rock, Paper, Scissors.
 playGame();
 
-//FUNCTION DEFINITION
-//Initialize a game of 5 rounds. Play 5 rounds, add the socre after each round, and determine the winner of the game.
+//FUNCTION DEFINITIONS
+//Initialize a game until player or computer reaches 5 wins.
 
 function playGame() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let getResult
-    //For each round, call functions to prompt the user for a choice and generate computer's choice. Store those variables and run
-    //them through playRound() to get the result of each round. Add 1 score to the winner of the round.
-    for (let i = 1; i <= 5; i++) {
-        console.log("Round " + i)
-        let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
-        getResult = playRound(playerSelection, computerSelection);
+    playerScore = 0;
+    computerScore = 0;
+
+    //Initialize event listener to read clicks on elements inside the button pad.
+    const btnPad = document.querySelector('#btnPad');
+
+    btnPad.addEventListener('click', (e) => {
+        let target = e.target;
+        let computerSelection;
+
+        switch (target.id) {
+            //Save player choice, call a function to get computer choice, update textContents and call playRound with both choices to return round winner to getResult.
+            case 'Rock':
+                console.log('Rock clicked');
+                playerSelection = 'Rock';
+                playerSelectionDisplay.textContent = 'Rock';
+                computerSelection = getComputerChoice();
+                computerSelectionDisplay.textContent = computerSelection;
+                getResult = playRound(playerSelection, computerSelection);
+                break;
+            case 'Paper':
+                console.log('Paper clicked');
+                playerSelection = 'Paper';
+                playerSelectionDisplay.textContent = 'Paper';
+                computerSelection = getComputerChoice();
+                computerSelectionDisplay.textContent = computerSelection;
+                getResult = playRound(playerSelection, computerSelection);
+                break;
+            case 'Scissors':
+                console.log('Scissors clicked');
+                playerSelection = 'Scissors';
+                playerSelectionDisplay.textContent = 'Scissors';
+                computerSelection = getComputerChoice();
+                computerSelectionDisplay.textContent = computerSelection;
+                getResult = playRound(playerSelection, computerSelection);
+                break;
+
+            //Reset button to clear saved values from previous game and reset btnPad with the player choice buttons.
+            case 'Reset':
+                playerScore = 0;
+                computerScore = 0;
+                playerSelectionDisplay.textContent = '';
+                computerSelectionDisplay.textContent = '';
+                resultText.textContent = '';
+                gameResultDisplay.textContent = '';
+
+
+                while (btnPad.firstChild) {
+                    btnPad.removeChild(btnPad.firstChild);
+                }
+                const Rock = document.createElement('button');
+                Rock.id = 'Rock';
+                Rock.textContent = "Rock";
+
+                const Paper = document.createElement('button');
+                Paper.id = 'Paper';
+                Paper.textContent = "Paper";
+
+                const Scissors = document.createElement('button');
+                Scissors.id = 'Scissors';
+                Scissors.textContent = "Scissors";
+
+                btnPad.appendChild(Rock);
+                btnPad.appendChild(Paper);
+                btnPad.appendChild(Scissors);
+                break;
+        }
+
+        //Add up round wins to score counters and update textContent values.
+        
         if (getResult === "Player wins the round") {
             playerScore++;
+            getResult = '';
         } else if (getResult === "Computer wins the round") {
             computerScore++;
-        } else if (getResult === "Error") {
-            //If we get "Error" after a round, substract 1 from the counter so the for loop will run 1 more time, efectively repeating the round
-            i--;
-        }
-    }
-    console.log("Player Score: " + playerScore + " | Computer Score: " + computerScore)
+            getResult = '';
 
-    if (playerScore > computerScore) {
-        console.log("PLAYER WINS");
-    } else {
-        console.log("COMPUTER WINS");
-    }
+        }
+
+
+        playerScoreDisplay.querySelector('span').textContent = playerScore;
+        computerScoreDisplay.querySelector('span').textContent = computerScore;
+
+
+        //Check for 5 wins to show winner text, add Reset button and remove choice buttons.
+        if (playerScore === 5 || computerScore === 5) {
+            let winner;
+            if (playerScore === 5) {
+                winner = "Player";
+            } else if (computerScore === 5) {
+                winner = "Computer";
+            }
+
+            gameResultDisplay.textContent = 'The winner is: ' + winner;
+
+            while (btnPad.firstChild) {
+                btnPad.removeChild(btnPad.firstChild);
+            }
+
+            const resetBtn = document.createElement('button');
+            resetBtn.id = 'Reset';
+            resetBtn.textContent = "Reset";
+
+            btnPad.appendChild(resetBtn);
+            return;
+        }
+    })
 }
 
 //Function: Generate and register computer's choice (Rock, Paper OR Scissors)
@@ -42,10 +134,10 @@ function getComputerChoice() {
     //If number < 0.333 register Rock.
     if (randomNumber < 0.333) {
         computerChoice = "Rock";
-    //If number >= 0.333 and < 0.666 register Paper.
+        //If number >= 0.333 and < 0.666 register Paper.
     } else if (randomNumber >= 0.333 && randomNumber < 0.666) {
         computerChoice = "Paper";
-    //If number >= 0.666 Register Scissors.
+        //If number >= 0.666 Register Scissors.
     } else if (randomNumber >= 0.666) {
         computerChoice = "Scissors";
     }
@@ -64,7 +156,7 @@ function getPlayerChoice() {
 //Function: Play a round - Input computer's choice and player's choice values, output the winner of the round. If passed unexpected arguments, return "Error".
 
 function playRound(playerSelection, computerSelection) {
-    let roundResult
+    let roundResult;
 
     //Compare computerSelection and playerSelection to determine the result of the round
     if (playerSelection === "Rock") {
@@ -96,6 +188,12 @@ function playRound(playerSelection, computerSelection) {
         console.log("Error: Input must be 'Rock', 'Paper' or 'Scissors'");
         roundResult = "Error";
     }
-    return roundResult
+
+    //Update textContent value.
+    resultText.textContent = roundResult;
+
+    return roundResult;
 }
+
+
 
